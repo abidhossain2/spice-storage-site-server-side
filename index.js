099a98d093c9ee1de88248fc25e085fcf6d8e0f9
abidhossain2 <tanvirabid229@gmail.com>
2022-04-30 01:36:01 +0600

    products api create

--- index.js
+++ index.js
@@ -11,11 +11,24 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.congk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        const ProductCollection = client.db("spice").collection("products");
+        app.get('/products', async(req, res) => {
+            const query = {};
+            const cursor = ProductCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Hello')
