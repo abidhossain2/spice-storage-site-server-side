0f29acee06545abd18e399ccff995db8c326345e
abidhossain2 <tanvirabid229@gmail.com>
2022-05-03 02:04:39 +0600

    email query

--- index.js
+++ index.js
@@ -15,11 +15,25 @@ async function run() {
     try {
         await client.connect();
         const ProductCollection = client.db("spice").collection("products");
-        console.log('db connected');
+        const MyItems = client.db("spice").collection("myitems")
+         console.log('db connected');
         app.get('/products', async(req, res) => {
             const query = {};
             const cursor = ProductCollection.find(query)
             const result = await cursor.toArray()
+            res.send(result);
+        })
+        app.get('/myitems', async(req, res) => {
+            const loginUser = req.query.email;
+            const query = {email: loginUser};
+            const cursor = MyItems.find(query)
+            const result = await cursor.toArray()
+            res.send(result);
+        })
+        app.post('/myitems', async(req, res) => {
+            const query =req.body;
+            const result = MyItems.insertOne(query)
+            // const result = await cursor.toArray()
             res.send(result);
         })
         app.post('/products', async(req, res) => {
@@ -36,11 +50,32 @@ async function run() {
             res.send(result)
         })
        
+        app.delete('/myitems/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await MyItems.deleteOne(query)
+            res.send(result)
+        })
+        
+       
 
         app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id;
             const query = {_id: ObjectId(id)}
             const result = await ProductCollection.findOne(query)
+            res.send(result)
+        })
+        // app.get('/inventory/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = {_id: ObjectId(id)}
+        //     const result = await MyItems.findOne(query)
+        //     res.send(result)
+        // })
+        
+        app.get('/myitems/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await MyItems.findOne(query)
             res.send(result)
         })
 
